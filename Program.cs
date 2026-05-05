c9bbf7be0cf2834fdbd8568907541b6eddfa383b
Sadullayev_o21 <muhammadziyosadullayev755@gmail.com>
2026-05-05 19:57:49 +0500

    Yangilandi

--- Program.cs
+++ Program.cs
@@ -5,3 +5,34 @@ int natija = 0;
 int hozirgiSon = 0;
 char amal = '+';
 int i = 0;
+while (i < input.Length)
+{
+                char belgi = input[i];
+
+                
+                if (belgi >= '0' && belgi <= '9')
+                {
+                    
+                    hozirgiSon = (hozirgiSon * 10) + (belgi - '0');
+                }
+                
+                
+                if (belgi == '+' || belgi == '-' || i == input.Length - 1)
+                {
+                    
+                    if (amal == '+')
+                    {
+                        natija = natija + hozirgiSon;
+                    }
+                    else if (amal == '-')
+                    {
+                        natija = natija - hozirgiSon;
+                    }
+
+                    
+                    amal = belgi;
+                    hozirgiSon = 0;
+                }
+
+                i++;
+}
