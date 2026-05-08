bf056ceb50210e4773c6a1f594329637f98f4244
Sadullayev_o21 <muhammadziyosadullayev755@gmail.com>
2026-05-08 20:04:37 +0500

    Yangilandi

--- Program.cs
+++ Program.cs
@@ -1,39 +1,50 @@
-﻿Console.Write("Ifodani kiriting (masalan, 1+5-2): ");
-string input = Console.ReadLine();
+﻿bool davomEtish = true; 
 
-int natija = 0;
-int hozirgiSon = 0;
-char amal = '+';
-int i = 0;
-while (i < input.Length)
+while (davomEtish)
 {
-    char belgi = input[i];
+    Console.Write("Ifodani kiriting (masalan, 1+5-2): ");
+    string input = Console.ReadLine();
 
-                if (belgi >= '0' && belgi <= '9')
-                {
-                    
-                    hozirgiSon = (hozirgiSon * 10) + (belgi - '0');
-                }
-                
-                
-                if (belgi == '+' || belgi == '-' || i == input.Length - 1)
-                {
-                    
-                if (amal == '+')
-                    {
-                        natija = natija + hozirgiSon;
-                    }
-                else if (amal == '-')
-                    {
-                        natija = natija - hozirgiSon;
-                    }
+    int natija = 0;
+    int hozirgiSon = 0;
+    char amal = '+';
+    int i = 0;
 
-                    
-                    amal = belgi;
-                    hozirgiSon = 0;
-                }
+    while (i < input.Length)
+    {
+        char belgi = input[i];
 
-                i++;
+        if (belgi >= '0' && belgi <= '9')
+        {
+            hozirgiSon = (hozirgiSon * 10) + (belgi - '0');
+        }
+
+        if (belgi == '+' || belgi == '-' || i == input.Length - 1)
+        {
+            if (amal == '+')
+            {
+                natija = natija + hozirgiSon;
+            }
+            else if (amal == '-')
+            {
+                natija = natija - hozirgiSon;
+            }
+
+            amal = belgi;
+            hozirgiSon = 0;
+        }
+        i++;
+    }
+
+    Console.WriteLine("Natija: " + natija);
+
+    Console.Write("\nYana hisoblashni xohlaysizmi? (ha/yo'q): ");
+    string javob = Console.ReadLine().ToLower();
+
+    if (javob != "ha")
+    {
+        davomEtish = false;
+    }
 }
 
-Console.WriteLine("Natija: " + natija);
+Console.WriteLine("Dastur tugadi. Xayr!");
