fdf35a73382bc147a975c2c1ee8579ad40b3baf3
Sadullayev_o21 <muhammadziyosadullayev755@gmail.com>
2026-05-11 19:55:26 +0500

    Yangilandi

--- Program.cs
+++ Program.cs
@@ -1,16 +1,50 @@
-﻿bool davomEtish = true; 
+﻿﻿using System;
 
-while (davomEtish)
+Console.WriteLine("Assalomu alaykum! CalString dasturiga xush kelibsiz!");
+
+string tanlov;
+
+do
+{
+    Console.WriteLine("\n--- MENYU ---");
+    Console.WriteLine("1. Ifodani hisoblash (+/-)");
+    Console.WriteLine("2. Eng katta sonni topish");
+    Console.WriteLine("3. Faktorial hisoblash");
+    Console.WriteLine("4. 1 dan N gacha yig'indi");
+    Console.WriteLine("0. Chiqish");
+    Console.Write("Tanlovingizni kiriting: ");
+    tanlov = Console.ReadLine();
+
+    if (tanlov == "1")
+    {
+        RunCalculator();
+    }
+    else if (tanlov == "2")
+    {
+        RunGetMax();
+    }
+    else if (tanlov == "3")
+    {
+        RunFactorial();
+    }
+    else if (tanlov == "4")
+    {
+        RunSumToN();
+    }
+
+} while (tanlov != "0");
+
+Console.WriteLine("Dastur tugadi. Xayr!");
+
+void RunCalculator()
 {
     Console.Write("Ifodani kiriting (masalan, 1+5-2): ");
     string input = Console.ReadLine();
-
     int natija = 0;
     int hozirgiSon = 0;
     char amal = '+';
-    int i = 0;
 
-    while (i < input.Length)
+    for (int i = 0; i < input.Length; i++)
     {
         char belgi = input[i];
 
@@ -21,30 +55,61 @@ while (davomEtish)
 
         if (belgi == '+' || belgi == '-' || i == input.Length - 1)
         {
-            if (amal == '+')
-            {
-                natija = natija + hozirgiSon;
-            }
-            else if (amal == '-')
-            {
-                natija = natija - hozirgiSon;
-            }
+            if (amal == '+') natija = natija + hozirgiSon;
+            else if (amal == '-') natija = natija - hozirgiSon;
 
             amal = belgi;
             hozirgiSon = 0;
         }
-        i++;
     }
-
     Console.WriteLine("Natija: " + natija);
-
-    Console.Write("\nYana hisoblashni xohlaysizmi? (ha/yo'q): ");
-    string javob = Console.ReadLine().ToLower();
-
-    if (javob != "ha")
-    {
-        davomEtish = false;
-    }
 }
 
-Console.WriteLine("Dastur tugadi. Xayr!");
+void RunGetMax()
+{
+    Console.Write("1-sonni kiriting: ");
+    int s1 = Convert.ToInt32(Console.ReadLine()); 
+    Console.Write("2-sonni kiriting: ");
+    int s2 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("3-sonni kiriting: ");
+    int s3 = Convert.ToInt32(Console.ReadLine());
+
+    int max = GetMax(s1, s2, s3);
+    Console.WriteLine("Eng katta son: " + max);
+}
+
+int GetMax(int a, int b, int c)
+{
+    int max = a;
+    if (b > max) max = b;
+    if (c > max) max = c;
+    return max;
+}
+
+void RunFactorial()
+{
+    Console.Write("Sonni kiriting: ");
+    int n = Convert.ToInt32(Console.ReadLine()); 
+    Console.WriteLine(n + "! = " + Factorial(n));
+}
+
+long Factorial(int n)
+{
+    long natija = 1;
+    for (int i = 1; i <= n; i++) natija *= i;
+    return natija;
+}
+
+void RunSumToN()
+{
+    Console.Write("N ni kiriting: ");
+    int n = Convert.ToInt32(Console.ReadLine()); 
+    Console.WriteLine("1 dan " + n + " gacha yig'indi: " + SumToN(n));
+}
+
+int SumToN(int n)
+{
+    int yigindi = 0;
+    for (int i = 1; i <= n; i++) yigindi += i;
+    return yigindi;
+}
